0d93b96beb425a0e3f86f4d0bd763ac3f9f54861
刘向明 <719327306@qq.com>
2018-05-23 18:05:05 +0800

    刘向明

--- javascript/jinghualei.js
+++ javascript/jinghualei.js
@@ -1,24 +1,40 @@
 // 刘向明
-function animation(obj,attr,iTarget,sudu){
+function animation(obj,opation,sudu,fn){
 	clearInterval(obj.time);
+	var speed=0;
+	var stop=false;				
 	obj.time = setInterval(function(){
-		var speed=0;
-		var dqz=parseFloat(getComputedStyle(obj,true)[attr]);
-		if(attr == 'opacity'){
-			dqz=dqz*100;
-		}
-		if (!sudu) {
-			if(dqz<=iTarget){
-				speed= 5;
-			}else{
-				speed= -5;
+		var stopall=true;
+		for(attr in opation){
+			var dqz=parseFloat(getComputedStyle(obj,true)[attr]);
+			if(attr == 'opacity'){
+				dqz=dqz*100;
 			}
-			if(Math.abs(iTarget-dqz)<Math.abs(speed)){
-				clearInterval(obj.time);
-				if(attr == 'opacity'){
-					obj.style[attr]=iTarget/100;
+			if (sudu) {
+				if(dqz<=opation[attr]){
+					speed= 10;
 				}else{
-					obj.style[attr] = iTarget+'px';
+					speed= -10;
+				}
+				if (Math.abs(opation[attr]-dqz)<Math.abs(speed)) {
+					stop=true;								
+				}else{
+					stopall=false;
+				}
+			}else{
+				speed=(opation[attr]-dqz)/20;
+				speed=speed>0 ? Math.ceil(speed) : Math.floor(speed);
+				if (!speed) {
+					stop=true;
+				}else{
+					stopall=false;
+				}
+			}					
+			if(stop){							
+				if(attr == 'opacity'){
+					obj.style[attr]=opation[attr]/100;
+				}else{
+					obj.style[attr] = opation[attr]+'px';
 				}
 			}else{
 				if(attr == 'opacity'){
@@ -27,19 +43,13 @@ function animation(obj,attr,iTarget,sudu){
 					obj.style[attr] = dqz+speed+'px';
 				}
 			}
-		}else{
-			speed=(iTarget-dqz)/10;
-			speed=speed>0 ? Math.ceil(speed) : Math.floor(speed);
-			if(speed == 0){
-				clearInterval(time);
-			}else{
-				if (attr=='opacity') {
-					obj.style[attr]= (dqz+speed)/100;
-				}else{
-					obj.style[attr] = dqz+speed+'px';
-				}						
+		}
+		if (stopall) {
+			clearInterval(obj.time);
+			if(fn){
+				fn();
 			}
-		}					
+		}
 	},30)
 }
 window.onscroll = window.onload = function(){
@@ -68,9 +78,9 @@ window.onscroll = window.onload = function(){
 	var oTop=document.getElementById('jstop');
 	var gHeight=document.body.scrollTop || document.documentElement.scrollTop;
 	if(gHeight>100){
-		oTop.style.display='block';
+		animation(oTop,{'gHeight':90});
 		oTop.style.top = gHeight+'px';
 	}else{
-		oTop.style.display='none';
+		animation(oTop,{'gHeight':0});
 	}
 }
