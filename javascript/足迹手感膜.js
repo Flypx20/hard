33f7eea414e426fd2f6ae7778eaa7b25cfa7fd54
鲤鱼IT <1776202826@qq.comomm>
2018-05-25 16:51:05 +0800

    顶部滑动&&购物车完成

--- javascript/足迹手感膜.js
+++ javascript/足迹手感膜.js
@@ -1,3 +1,4 @@
+
 function animation(obj,attr,iTarget){
 	clearInterval(obj.timer);
 	obj.timer=setInterval(function(){
@@ -33,11 +34,57 @@ window.onscroll=function(){
 		if(!isShow){
 			animation(oDiv,'height',60);
 			isShow=true;
+			// oDiv.style.borderBottom="1px solid #ccc";
+			// oDiv.style.boxShdow="0px 2px 2px #ccc";
 		}
 	}else{
 		if(isShow){
 			animation(oDiv,'height',0);
 			isShow=false;
 		}
+	}
+}
+gouWu();
+//购物车
+function gouWu(){
+	var oGouB=document.querySelector(".divb");
+	var oGouW=document.querySelector(".topr .a2");
+	var timer=null;
+	oGouW.onmouseenter=function(){
+		clearTimeout(timer);
+		oGouB.style.display='block';
+	}
+	oGouW.onmouseleave=function(){
+		timer=setTimeout(function(){
+			oGouB.style.display='none';
+		},300)
+	}
+	oGouB.onmouseenter=function(){
+		clearTimeout(timer);
+	}
+	oGouB.onmouseleave=function(){
+		oGouB.style.display="none";
+	}
+}
+// 下拉列表
+headerList1();
+function headerList1(){
+	var oPhoneLi=document.getElementById('phoneA');
+	var oHeaderList=document.getElementById('headerList');
+	var timer=null;
+	oPhoneLi.onmouseenter=function(){
+		clearTimeout(timer);
+		oHeaderList.style.display="block";
+	}
+	oPhoneLi.onmouseleave=function(){
+		timer=setTimeout(function(){
+			oHeaderList.style.display="none";
+		},300)
+	}
+	oHeaderList.onmouseenter=function(){
+		clearTimeout(timer);
+	}
+	oHeaderList.onmouseleave=function(){
+		oHeaderList.style.display="none";
 	}
 }
